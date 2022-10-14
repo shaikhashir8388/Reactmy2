04c66f6ea0f197d5051e70f4402b8c3179e2a815
shaikhashir8388 <115325745+shaikhashir8388@users.noreply.github.com>
2022-10-14 12:36:55 -0700

    Edit code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './style.css';
+import UseState from './useState';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hfu</h1>
+      <UseState/>
+    </div>
+  );
+}
+
+export default App;
